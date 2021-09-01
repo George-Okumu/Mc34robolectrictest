4d50ce9c2f75e239d73e36fa1368bd5e8954f513
George Okumu <gokumu12@gmail.com>
2021-09-01 13:12:44 +0300

    Used Robolectric shadows to test intent of actvities.

--- app/src/main/java/com/moringa/mc34robolectrictest/MainActivity.java
+++ app/src/main/java/com/moringa/mc34robolectrictest/MainActivity.java
@@ -2,13 +2,28 @@ package com.moringa.mc34robolectrictest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Button registerActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registerActivityButton = findViewById(R.id.secondActivityButton);
+        registerActivityButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == registerActivityButton){
+            Intent registerActivityIntent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(registerActivityIntent);
+        }
     }
 }
--- app/src/test/java/com/moringa/mc34robolectrictest/MainActivityTest.java
+++ app/src/test/java/com/moringa/mc34robolectrictest/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.moringa.mc34robolectrictest;
 
+import android.content.Intent;
 import android.os.Build;
 import android.widget.TextView;
 
@@ -8,11 +9,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-//This configures your android to ignore the api version.
+//This configures your android to ignore the 29 and above api version.
 @Config(sdk = {Build.VERSION_CODES.O_MR1})
 
 @RunWith(RobolectricTestRunner.class)
@@ -27,9 +32,21 @@ public class MainActivityTest {
                 .get();
     }
 
+    //Simple Robolectric test
     @Test
     public void confirmIfTexViewIsthere(){
         TextView mainactivitytextview = mainActivityTest.findViewById(R.id.mainActivityTextView);
+        assertNotNull("The Hello text view could not be found", mainactivitytextview);
         assertTrue("Hello World!".equals(mainactivitytextview.getText().toString()));
+    }
+    //Robolectric test using shadows
+    //Shadows are Classes that modify or extend the behavior of a class in the Android SDK.
+    @Test
+    public void confirmIfNextActivityButton_onClickWorks(){
+        mainActivityTest.findViewById(R.id.secondActivityButton).performClick();
+        Intent expectedIntent = new Intent(mainActivityTest, RegisterActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(mainActivityTest);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
     }
 }
